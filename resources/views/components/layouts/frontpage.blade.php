248f64c2beddf790ca42b208be1a766d1a13b8af
Alex <alex.l.luong@gmail.com>
2025-04-10 15:03:30 +0930

    Added icon support and header bar to frontpage

--- resources/views/components/layouts/frontpage.blade.php
+++ resources/views/components/layouts/frontpage.blade.php
@@ -3,8 +3,27 @@
 <head>
     @include('partials.head.frontpage')
 </head>
-<body class="min-h-screen">
-<x-mary-main>
+<body class="min-h-screen prose max-w-none">
+{{-- The navbar with `sticky` and `full-width` --}}
+<x-mary-nav full-width>
+
+    <x-slot:brand>
+        {{-- Drawer toggle for "main-drawer" --}}
+        <label for="main-drawer" class="lg:hidden mr-3">
+            <x-icon name="heroicon-o-arrow-left" class="cursor-pointer"/>
+        </label>
+
+        {{-- Brand --}}
+        <div>{{ $title ?? 'Pic Praiser' }}</div>
+    </x-slot:brand>
+
+    {{-- Right side actions --}}
+    <x-slot:actions>
+        <x-mary-button label="Login" link="{{ route('login') }}" class="btn-ghost btn-sm" responsive/>
+    </x-slot:actions>
+</x-mary-nav>
+
+<x-mary-main with-nav full-width>
     <x-slot:content>
         {{ $slot }}
     </x-slot:content>
