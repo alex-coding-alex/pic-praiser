a4a821018066291bfaf72fd7b3851cce60f6986d
Alex <alex.l.luong@gmail.com>
2025-04-11 12:16:14 +0930

    Show post image with user name avatar and post name

--- resources/views/livewire/posts/post-view.blade.php
+++ resources/views/livewire/posts/post-view.blade.php
@@ -1,15 +1,22 @@
 <section>
     <h1>View Upload</h1>
-    <p>
-        {{ $post->name }}
-    </p>
 
 
     @if($post->hasMedia(\App\Models\Post::IMAGE_COLLECTION))
-        <img
-            src="{{ $post->getFirstMedia(\App\Models\Post::IMAGE_COLLECTION)->getTemporaryUrl(\Illuminate\Support\Carbon::now()->addDay()) }}"
-            alt="Post Image"/>
+        <div class="hero">
+            <div class="relative">
+                <img
+                    src="{{ $post->getFirstMedia(\App\Models\Post::IMAGE_COLLECTION)->getTemporaryUrl(\Illuminate\Support\Carbon::now()->addDay()) }}"
+                    alt="Post Image">
 
+                <div class="z-10 absolute top-0 right-0 p-4">
+                    <x-mary-avatar :title="$post->user->name"/>
+                </div>
+                <p class="z-10 absolute bottom-0 p-4">{{ $post->name }}</p>
+            </div>
+        </div>
+    @else
+        <p class="text-error">No Image Found. Something went wrong</p>
     @endif
 
     <section>
